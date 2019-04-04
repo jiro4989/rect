eea220508a75776427c2362fdc96d513ead8b6b7
jiro4989 <jiroron666@gmail.com>
2019-04-04 16:09:09 +0900

    :sparkles: もうちょい細かく書いた

--- rect/rect.go
+++ rect/rect.go
@@ -1,5 +1,7 @@
 package rect
 
+import "github.com/mattn/go-runewidth"
+
 type PasteConfig struct {
 	X                int
 	Y                int
@@ -13,15 +15,46 @@ func Paste(src, inputData []string, config PasteConfig) (ret []string) {
 }
 
 func PasteLine(src, inputData string, config PasteConfig) string {
+	// マルチバイト文字を扱うためにruneに変換
 	srcRune := []rune(src)
 	inputRune := []rune(inputData)
+	// srcを直接上書きするためにrangeを使わない
 	for i := 0; i < len(inputRune); i++ {
 		ir := inputRune[i]
+		// 座標ズレ分を加算
+		// 直接i2を加算するとループ回数が変わってしまうため一時変数を使用
 		i2 := i + config.X
+
+		// 上書き元のテキストの範囲超過していたら空文字を追加
 		if len(srcRune) <= i2 {
 			srcRune = append(srcRune, ' ')
 		}
+		sr := srcRune[i2]
+		isFullWidth := runewidth.StringWidth(string(sr)) == 2
+		if isFullWidth {
+			// 変更対象が全角かつ、上書きに使う値が全角のときはそのまま置き換える
+			srcRune[i2] = ir
+
+			// 変更対象が全角かつ、上書きに使う値が半角のときは
+			if runewidth.StringWidth(string(ir)) == 2 {
+				continue
+			}
+			nextIndex := i2 + 1
+			if nextIndex < len(srcRune) {
+				srcRune[i2+1] = ir
+			}
+			continue
+		}
+
+		// 変更対象が半角のとき、上書きに使う値が全角だった場合は
+		// その次の値も更新する
 		srcRune[i2] = ir
+		if runewidth.StringWidth(string(ir)) == 2 {
+			nextIndex := i2 + 1
+			if nextIndex < len(srcRune) {
+				srcRune = append(srcRune[:nextIndex], srcRune[nextIndex+1:]...)
+			}
+		}
 	}
 	return string(srcRune)
 }
--- rect/rect_test.go
+++ rect/rect_test.go
@@ -1,6 +1,7 @@
 package rect
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -8,20 +9,27 @@ import (
 
 func TestPasteLine(t *testing.T) {
 	type TestData struct {
-		expect, src, inputData string
-		config                 PasteConfig
+		desc, expect, src, inputData string
+		config                       PasteConfig
 	}
 	testdatas := []TestData{
-		{expect: "abc45", src: "12345", inputData: "abc", config: PasteConfig{}},
-		{expect: "1abc5", src: "12345", inputData: "abc", config: PasteConfig{X: 1}},
-		{expect: "123abc", src: "12345", inputData: "abc", config: PasteConfig{X: 3}},
+		{desc: "半角のみ原点座標開始", expect: "abc45", src: "12345", inputData: "abc", config: PasteConfig{}},
+		{desc: "半角のみ原点+1X座標開始", expect: "1abc5", src: "12345", inputData: "abc", config: PasteConfig{X: 1}},
+		{desc: "半角のみ原点ズレによる元テキストの範囲超過", expect: "123abc", src: "12345", inputData: "abc", config: PasteConfig{X: 3}},
+		{desc: "全角あり原点座標開始", expect: "あい5", src: "12345", inputData: "あい", config: PasteConfig{}},
+		{desc: "全角あり原点+1X座標開始", expect: "1あい", src: "12345", inputData: "あい", config: PasteConfig{X: 1}},
+		{desc: "全角が全角を置き換える", expect: "あいお", src: "うえお", inputData: "あい", config: PasteConfig{}},
+		{desc: "全角が元テキストの範囲超過", expect: "あい", src: "お", inputData: "あい", config: PasteConfig{}},
+		//{desc: "全角は半角２文字分で全角１文字分ずれる", expect: "おあい", src: "お", inputData: "あい", config: PasteConfig{X: 2}},
 	}
 	for _, v := range testdatas {
 		got := PasteLine(v.src, v.inputData, v.config)
 		if diff := cmp.Diff(v.expect, got); diff != "" {
-			t.Error("NG\n" + diff)
+			msg := fmt.Sprintf("NG %s\n%s", v.desc, diff)
+			t.Error(msg)
 		} else {
-			t.Log("OK")
+			msg := fmt.Sprintf("OK %s", v.desc)
+			t.Log(msg)
 		}
 	}
 }
