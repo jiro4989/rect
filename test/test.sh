001296a2cc22a018181a39f2273567ca6533aa74
jiro4989 <jiroron666@gmail.com>
2019-04-05 08:47:15 +0900

    :sparkles: テスト追加

--- test/test.sh
+++ test/test.sh
@@ -3,6 +3,8 @@
 set -eu
 
 make build
+./bin/rect paste -X 10 <(echo -e '123\n456') <(echo -e 'abc')
+echo ---
 ./bin/rect paste -X 1 -Y 2 <(echo -e 'あ3\n456\n789') <(echo -e 'abcd\nefgh\nabc')
 echo ---
 ./bin/rect paste <(echo -e 'あ3\n456\n789') <(echo -e 'あ3d\nefgh\nabc')
