31aa91a0eac57d7b021eea8317820b24488f0702
jiro4989 <jiroron666@gmail.com>
2019-05-23 23:02:36 +0900

    :construction: 一旦コミット

--- src/rect/util.nim
+++ src/rect/util.nim
@@ -1,16 +1,47 @@
+import eastasianwidth
+
 import unicode
+from sequtils import repeat
+from strutils import join
+
+type
+  ClassifiedString* = object
+    data*: string
+    indices*: seq[int]
+
+proc toClassifiedString(s: string): seq[ClassifiedString] =
+  var i: int
+  for v in s.toRunes:
+    let w = v.`$`.stringWidth
+    var seqi: seq[int]
+    for j in i..<i+w:
+      seqi.add j
+    result.add ClassifiedString(data: $v, indices: seqi)
+    i += w
+
+proc pad(src, dst: var string, x = 0) =
+  dst = " ".repeat(x).join & dst
+  let diff = dst.stringWidth - src.stringWidth
+  src = src & " ".repeat(diff).join
 
 proc paste*(src, dst: seq[string], x, y: int): seq[string] =
   discard
 
 proc pasteLine*(dst, src: string, x = 0): string =
   var rets = dst.toRunes
-  for i, v in src.toRunes:
-    let n = i + x
+  let srcRunes = src.toRunes
+  var managePos: int
+  for i, _ in srcRunes:
+    let
+      i2 = i + managePos
+      v = srcRunes[i2]
+      n = i2 + x
     if rets.len <= n:
       let diff = n + 1 - rets.len
       for _ in 1..diff:
         rets.add Rune(' ')
     rets[n] = v
+    if v.`$`.stringWidth == 2:
+      inc managePos
   result = $rets
   
--- tests/test1.nim
+++ tests/test1.nim
@@ -2,6 +2,37 @@ import unittest
 
 include rect/util
 
+suite "toClassifiedString":
+  test "Half width":
+    check "123".toClassifiedString == @[ClassifiedString(data: "1", indices: @[0]), ClassifiedString(data: "2", indices: @[1]), ClassifiedString(data: "3", indices: @[2])]
+    check " 23".toClassifiedString == @[ClassifiedString(data: " ", indices: @[0]), ClassifiedString(data: "2", indices: @[1]), ClassifiedString(data: "3", indices: @[2])]
+  test "Full width":
+    check "あい".toClassifiedString == @[ClassifiedString(data: "あ", indices: @[0, 1]), ClassifiedString(data: "い", indices: @[2, 3])]
+    check "1い2う".toClassifiedString == @[ClassifiedString(data: "1", indices: @[0]), ClassifiedString(data: "い", indices: @[1, 2]), ClassifiedString(data: "2", indices: @[3]), ClassifiedString(data: "う", indices: @[4, 5])]
+
+suite "pad":
+  test "Half width":
+    var
+      a = "a"
+      b = "bbbbb"
+    pad(a, b)
+    check a == "a    "
+    check b == "bbbbb"
+  test "Half width, set pos":
+    var
+      a = "a"
+      b = "bbbbb"
+    pad(a, b, 2)
+    check a == "a      "
+    check b == "  bbbbb"
+  test "Full width, set pos":
+    var
+      a = "a"
+      b = "あいう"
+    pad(a, b, 1)
+    check a == "a      "
+    check b == " あいう"
+
 suite "paste":
   test "normal":
     discard
@@ -16,3 +47,6 @@ suite "pasteLine":
     check "123".pasteLine("abcde", x = 3) == "123abcde"
     check "123".pasteLine("abcde", x = 4) == "123 abcde"
     check "".pasteLine("abcde", x = 4) == "    abcde"
+  test "Full width":
+    check "".pasteLine("あいうえお") == "あいうえお"
+    check "123".pasteLine("あい") == "あ3"
