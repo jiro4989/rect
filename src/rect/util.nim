12fa32f96e48b44b48cfed62b4c9c653e11e7f97
jiro4989 <jiroron666@gmail.com>
2019-05-24 00:16:30 +0900

    :construction: 一旦コミット

--- src/rect/util.nim
+++ src/rect/util.nim
@@ -1,7 +1,7 @@
 import eastasianwidth
 
 import unicode
-from sequtils import repeat
+from sequtils import repeat, mapIt, filterIt
 from strutils import join
 
 type
@@ -24,24 +24,50 @@ proc pad(src, dst: var string, x = 0) =
   let diff = dst.stringWidth - src.stringWidth
   src = src & " ".repeat(diff).join
 
+proc continuedInts(s, e: int): seq[int] =
+  for v in s..e:
+    result.add v
+
+proc split3(self: openArray[ClassifiedString], minIndex, maxIndex: int): array[3, seq[ClassifiedString]] =
+  let
+    lastElem = self[self.len-1].indices
+    leftIndicesRange   = continuedInts(0, minIndex-1)
+    centerIndicesRange = continuedInts(minIndex, maxIndex)
+    rightIndicesRange  = continuedInts(maxIndex+1, lastElem[lastElem.len - 1])
+  result[0] = self.filterIt(0 < it.indices.filterIt(it in leftIndicesRange).len)
+  result[1] = self.filterIt(0 < it.indices.filterIt(it in centerIndicesRange).len)
+  result[2] = self.filterIt(0 < it.indices.filterIt(it in rightIndicesRange).len)
+  block:
+    var v: seq[ClassifiedString]
+    for r in result[0]:
+      if r notin result[1]:
+        v.add r
+    result[0] = v
+  block:
+    var v: seq[ClassifiedString]
+    for r in result[2]:
+      if r notin result[1]:
+        v.add r
+    result[2] = v
+
 proc paste*(src, dst: seq[string], x, y: int): seq[string] =
   discard
 
 proc pasteLine*(dst, src: string, x = 0): string =
-  var rets = dst.toRunes
-  let srcRunes = src.toRunes
-  var managePos: int
-  for i, _ in srcRunes:
-    let
-      i2 = i + managePos
-      v = srcRunes[i2]
-      n = i2 + x
-    if rets.len <= n:
-      let diff = n + 1 - rets.len
-      for _ in 1..diff:
-        rets.add Rune(' ')
-    rets[n] = v
-    if v.`$`.stringWidth == 2:
-      inc managePos
-  result = $rets
+  var
+    dst2 = dst
+    src2 = src
+  pad(dst2, src2, x)
+  var
+    dst3 = dst2.toClassifiedString
+    src3 = src2.toClassifiedString
+
+  let
+    minIndex    = src3[0].indices[0]
+    lastIndices = src3[src3.len-1].indices
+    maxIndex    = lastIndices[lastIndices.len-1]
+    s3 = dst3.split3(minIndex, maxIndex)
+    left   = s3[0]
+    center = s3[1]
+    right  = s3[2]
   
--- tests/test1.nim
+++ tests/test1.nim
@@ -33,20 +33,44 @@ suite "pad":
     check a == "a      "
     check b == " あいう"
 
+suite "continuedInts":
+  test "normal":
+    check continuedInts(0, 2) == @[0, 1, 2]
+    check continuedInts(1, 3) == @[1, 2, 3]
+
+suite "split3":
+  let data = @[ClassifiedString(indices: @[0, 1]), ClassifiedString(indices: @[2, 3]), ClassifiedString(indices: @[4, 5])]
+  test "center":
+    check data.split3(2, 3) == [@[ClassifiedString(indices: @[0, 1])], @[ClassifiedString(indices: @[2, 3])], @[ClassifiedString(indices: @[4, 5])]]
+  test "left center":
+    check data.split3(2, 4) == [@[ClassifiedString(indices: @[0, 1])], @[ClassifiedString(indices: @[2, 3]), ClassifiedString(indices: @[4, 5])], @[]]
+  test "center right":
+    check data.split3(1, 3) == [@[], @[ClassifiedString(indices: @[0, 1]), ClassifiedString(indices: @[2, 3])], @[ClassifiedString(indices: @[4, 5])]]
+  test "center only":
+    check data.split3(0, 4) == [@[], @[ClassifiedString(indices: @[0, 1]), ClassifiedString(indices: @[2, 3]), ClassifiedString(indices: @[4, 5])], @[]]
+    check data.split3(0, 5) == [@[], @[ClassifiedString(indices: @[0, 1]), ClassifiedString(indices: @[2, 3]), ClassifiedString(indices: @[4, 5])], @[]]
+
+  let data2 = @[ClassifiedString(indices: @[0, 1]), ClassifiedString(indices: @[2, 3]), ClassifiedString(indices: @[4, 5]), ClassifiedString(indices: @[6, 7])]
+  test "center":
+    check data2.split3(2, 4) == [@[ClassifiedString(indices: @[0, 1])], @[ClassifiedString(indices: @[2, 3]), ClassifiedString(indices: @[4, 5])], @[ClassifiedString(indices: @[6, 7])]]
+    check data2.split3(2, 5) == [@[ClassifiedString(indices: @[0, 1])], @[ClassifiedString(indices: @[2, 3]), ClassifiedString(indices: @[4, 5])], @[ClassifiedString(indices: @[6, 7])]]
+  test "left center":
+    check data2.split3(2, 6) == [@[ClassifiedString(indices: @[0, 1])], @[ClassifiedString(indices: @[2, 3]), ClassifiedString(indices: @[4, 5]), ClassifiedString(indices: @[6, 7])], @[]]
+
 suite "paste":
   test "normal":
     discard
 
-suite "pasteLine":
-  test "Half width":
-    check "".pasteLine("abcde") == "abcde"
-    check "123".pasteLine("abcde") == "abcde"
-  test "Half width, set X pos":
-    check "".pasteLine("abcde", x = 1) == " abcde"
-    check "123".pasteLine("abcde", x = 1) == "1abcde"
-    check "123".pasteLine("abcde", x = 3) == "123abcde"
-    check "123".pasteLine("abcde", x = 4) == "123 abcde"
-    check "".pasteLine("abcde", x = 4) == "    abcde"
-  test "Full width":
-    check "".pasteLine("あいうえお") == "あいうえお"
-    check "123".pasteLine("あい") == "あ3"
+# suite "pasteLine":
+#   test "Half width":
+#     check "".pasteLine("abcde") == "abcde"
+#     check "123".pasteLine("abcde") == "abcde"
+#   test "Half width, set X pos":
+#     check "".pasteLine("abcde", x = 1) == " abcde"
+#     check "123".pasteLine("abcde", x = 1) == "1abcde"
+#     check "123".pasteLine("abcde", x = 3) == "123abcde"
+#     check "123".pasteLine("abcde", x = 4) == "123 abcde"
+#     check "".pasteLine("abcde", x = 4) == "    abcde"
+#   test "Full width":
+#     check "".pasteLine("あいうえお") == "あいうえお"
+#     check "123".pasteLine("あい") == "あ3"
